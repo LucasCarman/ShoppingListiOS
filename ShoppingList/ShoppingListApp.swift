ea938c12f8ced636a823f3b5e2a5e43eb0d6c741
Lucas Carman <lucas.carman23@gmail.com>
2023-03-29 18:01:00 -0400

    Initial Commit

---
+++ ShoppingList/ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList
+//
+//  Created by Lucas  Carman  on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
